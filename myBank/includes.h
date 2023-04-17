01d6e435c7ba0d5567c035f58a615d2feccb8756
m1nato64 <vice.erik@mail.ru>
2023-04-17 17:11:29 +0300

    Added includes and do a first part of registration form

---
+++ myBank/includes.h
@@ -0,0 +1,18 @@
+// Includes
+// ------------------
+// 17.04.2023
+// Created by m1nato64
+
+#pragma once
+
+#include <iostream>
+#include <cmath>
+#include <Windows.h>
+#include <fstream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <conio.h>
+
+using namespace std;
---
+++ myBank/myBank.cpp
@@ -0,0 +1,33 @@
+﻿// Registration Form
+// ------------------
+// 17.04.2023
+// Created by m1nato64
+
+#include "includes.h"
+#include <limits>
+using namespace std;
+
+class registationUser {
+    public:
+        string username;
+        string password;
+        int age;
+};
+
+int main() {
+    registationUser user;       // Экземпляр для обращения.
+    cout << "Enter a username: ";
+    cin >> user.username;
+
+    cout << "Enter password: ";
+    cin >> user.password;
+
+    cout << "Enter age: ";
+    cin >> user.age;
+
+        // Выводим введенные данные на экран
+        cout << "Username: " << user.username << endl;
+        cout << "Password: " << user.password << endl;
+        cout << "Age: " << user.age << " years old" << endl;
+
+    }
